610a73cec49064437662bbe0cc2a772e0301d470
K-O-N <katenwankwo6@gmail.com>
2024-12-01 00:46:19 +0000

    seed files

--- bike_share/models/mart/merged.sql
+++ bike_share/models/mart/merged.sql
@@ -1,6 +1,8 @@
-select stg_location.location_id, region, country, population, density,
-           vehicle_id,	vehicle_type,	make_id,	model_year,	vehicle_desc,	
+select stg_location.location_id, region, country, population, density, stg_stolen_vehicles.make_id,
+           vehicle_id,	vehicle_type, model_year,	vehicle_desc,	
            color,	date_stolen
 from {{ref('stg_stolen_vehicles')}}
-left join {{ref('stg_location')}}
-ON stg_stolen_vehicles.location_id = stg_location.location_id
+left join {{ref('stg_location')}} 
+ON stg_stolen_vehicles.location_id = stg_location.location_id 
+LEFT JOIN {{ref('make_details')}} 
+ON stg_stolen_vehicles.make_id = make_details.make_id 
